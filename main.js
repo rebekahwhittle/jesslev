8c9b9be129970ec78560edeeca26f9add9079faa
rebekahwhittle <whittlebekah@gmail.com>
2019-10-09 18:45:22 -0500

    movement across pages and content change 🎉

--- main.js
+++ main.js
@@ -2,12 +2,36 @@
 $('#about').on('click', function(e){
     $('.container').attr('class', 'container');
     $('.container').addClass("about");
+    pageVisible();
 });
 $('#work').on('click', function(e){
     $('.container').attr('class', 'container');
     $('.container').addClass("work");
+    pageVisible();
 });
 $('#contact').on('click', function(e){
     $('.container').attr('class', 'container');
     $('.container').addClass("contact");
+    pageVisible();
 });
+
+function pageVisible  () {
+    if ($('.container').hasClass("about")) {
+        $('#about-page').addClass("visible");
+
+        $('#work-page').removeClass("visible").addClass("hidden");
+        $('#contact-page').removeClass("visible").addClass("hidden");
+    }
+    if ($('.container').hasClass("work")) {
+        $('#work-page').addClass("visible");
+
+        $('#about-page').removeClass("visible").addClass("hidden");
+        $('#contact-page').removeClass("visible").addClass("hidden");
+    }
+    if ($('.container').hasClass("contact")) {
+        $('#contact-page').addClass("visible");
+
+        $('#about-page').removeClass("visible").addClass("hidden");
+        $('#work-page').removeClass("visible").addClass("hidden");
+    }
+}
