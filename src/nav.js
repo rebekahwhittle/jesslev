35ae2dc3d9f0dfd5013515aad4041f7516b0f25b
Rebekah Whittle <whittlebekah@gmail.com>
2020-04-13 19:26:56 -0500

    navigation component

--- src/nav.js
+++ src/nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './App.css';
+import './styles/main.css';
 
 function Nav() {
   return (
@@ -18,7 +18,6 @@ function Nav() {
             </nav>
         </div> 
       </div>
-
   );
 }
 
